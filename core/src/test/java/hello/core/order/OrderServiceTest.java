21c67f0d169df274b906f0177dc65375f822184a
hyeonah <hyeona0000@naver.com>
2023-02-22 17:45:27 +0900

    230222 lec 7-2

--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -29,7 +29,7 @@ public class OrderServiceTest {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
-        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+        //Order order = orderService.createOrder(memberId, "itemA", 10000);
+        //Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 }
