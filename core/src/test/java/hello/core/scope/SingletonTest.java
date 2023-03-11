eacace161ee56cd01d8f983c1002760be69ae812
hyeonah <hyeona0000@naver.com>
2023-03-11 23:54:44 +0900

    lec 9-1

---
+++ core/src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,27 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonTest {
+    @Test
+    void singletonBeanFind() {
+        new AnnotationConfigApplicationContext(SingletonBean.class);
+    }
+    @Scope("singleton")
+    static class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
