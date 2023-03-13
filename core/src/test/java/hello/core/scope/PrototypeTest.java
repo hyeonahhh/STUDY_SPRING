685b25bdd81553d356dabcec9b56c92eefce75f8
hyeonah <hyeona0000@naver.com>
2023-03-14 07:59:28 +0900

    lec 9-2

---
+++ core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,42 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeTest {
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototypeBean1");
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototypeBean2");
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        prototypeBean1.destroy();
+        prototypeBean2.destroy();
+
+        ac.close();//해도 detroy 함수 실행 안됨.
+    }
+
+    //여기에 @Component가 없어도 컴포넌트 스캔이랑 비슷하게 동작하여 AnnotationConfigApplicationContext에 적은 클래스 자체를 빈으로 등록해주기때문에 괜찮음.
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
--- core/src/test/java/hello/core/scope/SingletonTest.java
+++ core/src/test/java/hello/core/scope/SingletonTest.java
@@ -1,5 +1,6 @@
 package hello.core.scope;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -10,7 +11,12 @@ import javax.annotation.PreDestroy;
 public class SingletonTest {
     @Test
     void singletonBeanFind() {
-        new AnnotationConfigApplicationContext(SingletonBean.class);
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        Assertions.assertThat(singletonBean1).isSameAs(singletonBean2);
     }
     @Scope("singleton")
     static class SingletonBean {
